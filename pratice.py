f915824d74e2d93aadd20617766f3fa0241611ca
pavanvikas36 <pavanvivek36@gmail.com>
2025-03-25 10:24:00 +0530

    Python problem using for loop

---
+++ pratice.py
@@ -0,0 +1,48 @@
+# for j in range(1, 11):
+#     for i in range(1, 31):
+#         print(j, i)
+
+# j = 1
+# while (j <= 10):
+#     i = 1
+#     while (i <= 30):
+#         print(j, i)
+#         i += 1
+#     j += 1
+
+# for j in range(1, 11):
+#     if (j % 2 != 0):
+#         for i in range(1, 31):
+#             print(j, i)
+
+# for j in range(1, 11):
+#     for i in range(1, 31):
+#         if (j % 2 != 0):
+#             print(j, i)
+
+# for i in range(1, 100 + 1):
+#     print(i)
+
+# sum = 0
+# for i in range(1, 100):
+#     sum = i*i+1/2
+# print(sum)
+
+# for i in range(1, 51):
+#     if (i % 2 == 0):
+#         print(i)
+
+# for i in range(1, 21):
+#     for j in range(1, 11):
+#         print(i , "X", j," = ",i * j)
+#     print("-----------**-----------")
+
+# sum = 0
+# n = 5
+# while (1 <= n):
+#     sum = sum + n
+#     print(n)
+#     n -= 1
+# print(sum)
+
+
