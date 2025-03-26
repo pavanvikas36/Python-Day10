3afc1930ef0c68ddf4dc3b85d361343f5717385a
pavanvikas36 <pavanvivek36@gmail.com>
2025-03-26 10:28:09 +0530

    Problem Solving in python using loops

--- pratice.py
+++ pratice.py
@@ -20,29 +20,35 @@
 #         if (j % 2 != 0):
 #             print(j, i)
 
+# 1) Print all numbers from 1 to 100 using a  for  loop. 
 # for i in range(1, 100 + 1):
 #     print(i)
 
+# 2) Write a program to print the sum of the first  n  natural numbers. (n*n+1/ 2) 
 # sum = 0
 # for i in range(1, 100):
 #     sum = i*i+1/2
 # print(sum)
 
+# 3) Print all even numbers between 1 and 50 using a  while loop. 
 # for i in range(1, 51):
 #     if (i % 2 == 0):
 #         print(i)
 
+# 4) Write a program to display the multiplication table of a given number. First 20 
 # for i in range(1, 21):
 #     for j in range(1, 11):
 #         print(i , "X", j," = ",i * j)
 #     print("-----------**-----------")
 
-# sum = 0
-# n = 5
-# while (1 <= n):
-#     sum = sum + n
-#     print(n)
-#     n -= 1
-# print(sum)
+# 5) Reverse a number using a  while  loop.
+# num1 = 54321
+# rev_num = 0
+# while num1 > 0:
+#     rem = num1 % 10
+#     print(rem)
+#     rev_num = rev_num * 10 + rem
+#     num1 = num1 // 10
+# print(rev_num)
 
 
